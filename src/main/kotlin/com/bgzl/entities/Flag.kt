fca6a230adf8ce481473ed27ec841e7bfcefa263
Andrey Mukamolow <fobo66@protonmail.com>
2018-08-20 01:21:52 +0300

    Filled Flag entity

--- src/main/kotlin/com/bgzl/entities/Flag.kt
+++ src/main/kotlin/com/bgzl/entities/Flag.kt
@@ -1,3 +1,47 @@
 package com.bgzl.entities
 
-data class Flag(val id: Int)
+/**
+ * Flag that can be set to bug or attachment. Defines bug status or status of the work about this bug
+ */
+data class Flag(
+
+    /**
+     * The ID of the flag.
+     */
+    val id: Int,
+
+    /**
+     * The name of the flag.
+     */
+    val name: String,
+
+    /**
+     * The type ID of the flag.
+     */
+    val typeId: Int,
+
+    /**
+     * The timestamp when this flag was originally created.
+     */
+    val creationTime: Long,
+
+    /**
+     * The timestamp when the flag was last modified.
+     */
+    val modificationDate: Long,
+
+    /**
+     * The current status of the flag such as ?, +, or -.
+     */
+    val status: String,
+
+    /**
+     * The login name of the user who created or last modified the flag.
+     */
+    val setter: String?,
+
+    /**
+     * The login name of the user this flag has been requested to be granted or denied. Note, this field is only returned if a requestee is set.
+     */
+    val requestee: String?
+)
